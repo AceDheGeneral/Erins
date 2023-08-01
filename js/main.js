e632ff475e071b40e344cdcc3105d238e2a2820d
Ace <117580257+AceDheGeneral@users.noreply.github.com>
2023-08-01 15:58:04 +0100

    1

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// let logout = document.querySelector('.logoutBtn');
+// logoutBtn=document.getElementById('logoutBtn')
+
+
+// function log(){
+//     window.location.href="main.html";
+//     var btn=document.getElementById("btn");
+//     btn.onclick=function(){
+//         alert("you are logged out");
+//     }
+// }
+// const logoutBtn=document.getElementById("logoutBtn");
+// logoutBtn.addEventListener("click",function(){
+
+// });
+// function books(){
+//     var books
+// }
+
+
+
+
+// IF ITS AN OFFICIAL WORK THEN IT IS NOT IMPORTANT BUT IF YOU ARE JUST TESTING THING OUT YOUR SELF THEN SURE.
