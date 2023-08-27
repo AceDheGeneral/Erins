d8cfa263ef095f664c058cba40c30e1552efd8fb
Ace <117580257+AceDheGeneral@users.noreply.github.com>
2023-08-27 16:17:48 +0100

    1.1

--- js/main.js
+++ js/main.js
@@ -22,3 +22,51 @@
 
 
 // IF ITS AN OFFICIAL WORK THEN IT IS NOT IMPORTANT BUT IF YOU ARE JUST TESTING THING OUT YOUR SELF THEN SURE.
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: true,
+    slidesPerView: 4,
+    fade: true,
+    loopFillGroupWithBlank: true,
+    gragCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 7,
+      },
+      550: {
+        spaceBetween: 10,
+      },
+      800: {
+        slidesPerView: 3,
+        spaceBetween: 25,
+      },
+      1114: {
+        slidesPerView: 4,
+      }
+    },
+    autoplay:{
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+  });
+  
